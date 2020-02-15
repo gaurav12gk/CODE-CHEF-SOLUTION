bad48c0f6f4b58f231f34093e830db54bea98e40
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2020-02-15 22:28:44 +0530

    Add files via upload

---
+++ snackdown year.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define lb long double
+#define ull unsigned long long int
+
+#define vi vector<int>
+# define vs vector < string >
+# define vb vector < bool >
+
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+
+
+# define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+using namespace std;
+void solve()
+{
+int year;
+cin>>year;
+if(year==2010||year==2015||year==2017||year==2019||year==2016)
+cout<<"HOSTED\n";
+else
+cout<<"NOT HOSTED\n";
+
+}
+
+int main()
+{fast
+int t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+
+}
